3e491a01db93c380807138228337102ffcf6893a
Pierre-Hugues Husson <phh@phh.me>
2020-04-29 00:17:01 +0200

    Revert "Ignore unknown audio codecs in media profiles' AudioEncoder Cap"
    This reverts commit ee00227dc5ddfc88b13fa37ba96f846950c03b4e.

--- media/libmedia/MediaProfiles.cpp
+++ media/libmedia/MediaProfiles.cpp
@@ -300,7 +300,6 @@ MediaProfiles::createAudioEncoderCap(const char **atts)
 
     const size_t nMappings = sizeof(sAudioEncoderNameMap)/sizeof(sAudioEncoderNameMap[0]);
     const int codec = findTagForName(sAudioEncoderNameMap, nMappings, atts[1]);
-    if(codec == -1) return nullptr;
     CHECK(codec != -1);
 
     MediaProfiles::AudioEncoderCap *cap =
@@ -420,9 +419,7 @@ MediaProfiles::startElementHandler(void *userData, const char *name, const char 
         profiles->mVideoEncoders.add(createVideoEncoderCap(atts));
     } else if (strcmp("AudioEncoderCap", name) == 0 &&
                strcmp("true", atts[3]) == 0) {
-	    MediaProfiles::AudioEncoderCap* cap = createAudioEncoderCap(atts);
-        if(cap != nullptr)
-            profiles->mAudioEncoders.add(cap);
+        profiles->mAudioEncoders.add(createAudioEncoderCap(atts));
     } else if (strcmp("VideoDecoderCap", name) == 0 &&
                strcmp("true", atts[3]) == 0) {
         profiles->mVideoDecoders.add(createVideoDecoderCap(atts));
